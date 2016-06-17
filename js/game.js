1a0d982a84f34f715ac37317776fe487ffcd891b
Poxymyr <julien.bouedron@gmail.com>
2016-06-17 12:22:27 +0200

    Cleaned up
    -Added requireJS
    -Added highlight asset
    -Redone the arrangement

--- js/game.js
+++ js/game.js
@@ -1,40 +1,47 @@
-var Siam = {
-  grid: {},
+require(['js/config.js'], function(config) {
+  var Siam = {
+    grid: {},
+    gridBorders: {},
 
-  preload: function() {
-    this.load.image('cell', './img/cell.png');
-    this.load.image('mountain', './img/mountain.png');
-    this.load.image('elephant', './img/player_blue.png');
-    this.load.image('rhino', './img/player_red.png');
-  },
+    preload: function() {
+      this.load.image('cell', './img/cell.png');
+      this.load.image('highlight', './img/highlight.png');
+      this.load.image('mountain', './img/mountain.png');
+      this.load.image('elephant', './img/player_blue.png');
+      this.load.image('rhino', './img/player_red.png');
+    },
 
-  create: function() {
-    this.grid = this.add.group();
+    create: function() {
+      this.grid = this.add.group();
+      this.gridBorders = this.add.group();
 
-    for(var c = 0; c < 5; c++) {
-      for(var r = 0; r < 5; r++) {
-        this.grid[c] = {};
-        this.grid[c][r] = this.add.sprite((c+2)*128, r*128, 'cell');
+      this.grid.x = config.TILE_SIZE*2;
+      this.grid.y = config.TILE_SIZE;
+
+      for(var c = 0; c < 5; c++) {
+        for(var r = 0; r < 5; r++) {
+          this.grid.create(c*config.TILE_SIZE, r*config.TILE_SIZE, 'cell');
+        }
       }
+
+      var sprite = null;
+      for(var i = 0; i < 5; i++) {
+        sprite = this.add.sprite(0, (i+1)*config.TILE_SIZE, 'elephant');
+        sprite.inputEnabled = true;
+        sprite.input.enableDrag();
+        sprite.input.enableSnap(config.TILE_SIZE, config.TILE_SIZE, false, true);
+        sprite.events.onDragStart.add(this.highlightGridBorders, sprite);
+      }
+    },
+
+    update: function() {},
+
+    highlightGridBorders: function() {
+      console.log(Siam);
     }
+  };
 
-    var sprite = null;
-    for(var i = 0; i < 5; i++) {
-      sprite = this.add.sprite(7.5 * 128, i*128, 'elephant');
-      sprite.inputEnabled = true;
-      sprite.input.enableDrag();
-      sprite.input.enableSnap(128, 128, false, true);
-      sprite.events.onDragStop.add(this.pawnRelease, sprite);
-    }
-  },
+  var game = new Phaser.Game(config.TILE_SIZE*9, config.TILE_SIZE*7);
 
-  update: function() {},
-
-  pawnRelease: function() {
-    console.log(Siam);
-  }
-};
-
-var game = new Phaser.Game(128*9, 128*5);
-
-game.state.add('Siam', Siam, true);
+  game.state.add('Siam', Siam, true);
+});
